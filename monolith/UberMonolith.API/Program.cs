f7aab5a671171a0b3794e300e5b12640aa474c90
SandraHany <sandra.nasry00@eng-st.cu.edu.eg>
2026-03-28 16:37:05 +0200

    added prometheus,loki, jaeger

--- monolith/UberMonolith.API/Program.cs
+++ monolith/UberMonolith.API/Program.cs
@@ -5,6 +5,11 @@ using UberMonolith.API.Mappings;
 using UberMonolith.API.Repositories;
 using StackExchange.Redis;
 using UberMonolith.API;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using Serilog.Sinks.Grafana.Loki;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString =
     builder.Configuration.GetConnectionString("PostgreSqlConnection")
@@ -12,12 +17,25 @@ var connectionString =
         + "'PostgreSqlConnection' not found.");
 
 var logger = new LoggerConfiguration()
-    .MinimumLevel.Debug()
+    .MinimumLevel.Verbose()
     .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.GrafanaLoki(
+        "http://localhost:3100",
+        labels: new List<LokiLabel>
+        {
+            new() { Key = "app",     Value = "uber-backend" },
+            new() { Key = "env",     Value = builder.Environment.EnvironmentName }
+        })
+    .Enrich.FromLogContext()
     .CreateLogger();
 
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(System.Net.IPAddress.Any, 5000);
+});
+
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -33,7 +51,33 @@ builder.Services.AddScoped<IRideRepository, RideRepository>();
 builder.Services.AddScoped<IDriverRepository,DriverRepository>();
 builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("RedisConnection")));
 builder.Services.AddScoped(c => c.GetRequiredService<IConnectionMultiplexer>().GetDatabase());
+var otel = builder.Services.AddOpenTelemetry();
 
+// Configure OpenTelemetry Resources with the application name
+otel.ConfigureResource(resource => resource
+    .AddService(serviceName: builder.Environment.ApplicationName));
+
+// Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+otel.WithMetrics(metrics => metrics
+    // Metrics provider from OpenTelemetry
+    .AddAspNetCoreInstrumentation()
+    // Metrics provides by ASP.NET Core in .NET 8
+    .AddMeter("Microsoft.AspNetCore.Hosting")
+    .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+    // Metrics provided by System.Net libraries
+    .AddMeter("System.Net.Http")
+    .AddMeter("System.Net.NameResolution")
+    .AddPrometheusExporter());
+otel.WithTracing(tracing => tracing
+    .AddAspNetCoreInstrumentation()
+    .AddEntityFrameworkCoreInstrumentation()  
+    .AddRedisInstrumentation()                
+    .AddOtlpExporter(opts =>
+    {
+        opts.Endpoint = new Uri("http://localhost:4317"); 
+    }));   
+
+                  
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -50,5 +94,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+// Configure the Prometheus scraping endpoint
+app.MapPrometheusScrapingEndpoint();
 app.Run();
